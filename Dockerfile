195cc97ca86d88713ac45dec1d3f0abaef00e1b8
Aragosu <aragos@bk.ru>
2024-03-22 09:35:59 +0300

    5 - финальное описсание проекта

--- Dockerfile
+++ Dockerfile
@@ -13,8 +13,6 @@ COPY . .
 RUN chmod a+x ./docker/*.sh
 
 # RUN alembic upgrade head
-
 #WORKDIR src
-
 #CMD gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
 CMD ./docker/app.sh
--- docker/app.sh
+++ docker/app.sh
@@ -4,8 +4,5 @@ alembic upgrade head
 
 cd src
 
-#cat database_creation.sql | psql -U postgres
-
-
 #gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 #убрал для деплоя render
 gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
