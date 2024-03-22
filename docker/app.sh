82dd48b42007f94a18e979ae7ec397e0c8f9565c
Aragosu <aragos@bk.ru>
2024-03-22 08:46:53 +0300

    4 - изменил worker в app

--- docker/app.sh
+++ docker/app.sh
@@ -6,4 +6,6 @@ cd src
 
 #cat database_creation.sql | psql -U postgres
 
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
+
+#gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 #убрал для деплоя render
+gunicorn main:app --workers 1 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
